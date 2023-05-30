c003654a02110bf6c3656b74e99017c760e6d1d9
a22fokol <a22fokol@student.his.se>
2023-05-31 00:33:45 +0200

    här är min UniversityDetailsActivity.java som har alla university detaljer

--- app/src/main/java/com/example/project/UniversityDetailsActivity.java
+++ app/src/main/java/com/example/project/UniversityDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -7,32 +8,43 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.squareup.picasso.Picasso;
+import com.example.project.University;
+
 public class UniversityDetailsActivity extends AppCompatActivity {
+
+    private TextView textViewName;
+    private TextView textViewLocation;
+    private TextView textViewSize;
+    private TextView textViewCost;
+    private TextView textViewCategory;
+    private TextView textViewCompany;
+    private TextView textViewId; // Declare textViewId
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_university_details);
 
-        // Retrieved the university object from the intent
-        Intent intent = getIntent();
-        Country university = (Country) intent.getSerializableExtra("university");
+        textViewName = findViewById(R.id.textViewName);
+        textViewLocation = findViewById(R.id.textViewLocation);
+        textViewSize = findViewById(R.id.textViewSize);
+        textViewCost = findViewById(R.id.textViewCost);
+        textViewCategory = findViewById(R.id.textViewCategory);
+        textViewCompany = findViewById(R.id.textViewCompany);
+        textViewId = findViewById(R.id.textViewId); // Initialize textViewId
 
-        // Use the university object to populate the details in the activity
-        TextView nameTextView = findViewById(R.id.nameTextView);
-        TextView locationTextView = findViewById(R.id.locationTextView);
-        TextView populationTextView = findViewById(R.id.populationTextView);
-        TextView descriptionTextView = findViewById(R.id.descriptionTextView);
-        ImageView imageView = findViewById(R.id.imageView);
-        TextView websiteTextView = findViewById(R.id.websiteTextView); // Add the website TextView in the layout
-
-        nameTextView.setText(university.getName());
-        locationTextView.setText(university.getLocation());
-        populationTextView.setText(String.valueOf(university.getPopulation()));
-        descriptionTextView.setText(university.getDescription());
-        websiteTextView.setText(university.getWebsite()); // Set the website TextView with the retrieved website
-
-        // Load the image using a library like Picasso or Glide
-        Picasso.get().load(university.getImageUrl()).into(imageView);
+        University university = getIntent().getParcelableExtra("university");
+        if (university != null) {
+            textViewName.setText("Name:"+university.getName());
+            textViewLocation.setText("Location: " +university.getLocation());
+            textViewSize.setText("Size: " + university.getSize());
+            textViewCost.setText("Cost: " + university.getCost());
+            textViewCategory.setText("Category: " + university.getCategory());
+            textViewCompany.setText("Company: " + university.getCompany());
+            textViewId.setText("ID: " + university.getId()); // Set the text for textViewId
+        } else {
+            // Handle the case where the University object is null
+            // You can display an error message or take appropriate action
+        }
     }
 }
