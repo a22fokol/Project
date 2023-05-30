c9314f024ec9345ca0814cc2504fe23215a236de
a22fokol <a22fokol@student.his.se>
2023-05-31 00:32:35 +0200

    här är min Recyclerview som behåller mina list items och visa också deras position in i appen

--- app/src/main/java/com/example/project/RecyclerAdapter.java
+++ app/src/main/java/com/example/project/RecyclerAdapter.java
@@ -3,23 +3,27 @@ package com.example.project;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.squareup.picasso.Picasso;
-
+import java.text.BreakIterator;
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
-    private ArrayList<Country> universityList;
+    private ArrayList<University> universityList;
     private OnItemClickListener listener;
 
-    public RecyclerAdapter(ArrayList<Country> universityList, OnItemClickListener listener) {
+    public RecyclerAdapter(ArrayList<University> universityList, OnItemClickListener listener) {
         this.universityList = universityList;
         this.listener = listener;
+    }
+
+    public void updateUniversityList(ArrayList<University> newUniversityList) {
+        universityList.clear();
+        universityList.addAll(newUniversityList);
+        notifyDataSetChanged();
     }
 
     @NonNull
@@ -31,19 +35,30 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Country university = universityList.get(position);
+        University university = universityList.get(position);
 
         // Set the university name
         holder.universityName.setText(university.getName());
 
-        // Set the university locations with population
-        String locationWithPopulation = university.getLocation() + " (Population: " + university.getPopulation() + ")";
-        holder.universityLocation.setText(locationWithPopulation);
+        // Set the university location
+        holder.universityLocation.setText(university.getLocation());
 
-        // Set the university image
-        Picasso.get().load(university.getImageUrl()).into(holder.universityImage);
+        // Set the university size
+        holder.universitySize.setText(String.valueOf(university.getSize()));
+
+        // Set the university cost
+        holder.universityCost.setText(String.valueOf(university.getCost()));
+
+        // Set the university category
+        holder.universityCategory.setText(university.getCategory());
+
+        // Set the university ID
+        holder.universityId.setText(university.getId());
+
+
+        // Set the university cateegory
+        holder.universityCompany.setText(university.getCompany());
     }
-
 
     @Override
     public int getItemCount() {
@@ -51,32 +66,42 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     }
 
     public interface OnItemClickListener {
-        void onItemClick(Country university);
+        void onItemClick(University university);
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView universityName;
         public TextView universityLocation;
-        public TextView universityPopulation;
-        public ImageView universityImage;
+        public TextView universitySize;
+        public TextView universityCost;
+        public TextView universityCategory;
+        public TextView universityId;
+        public TextView universityCompany;
 
         public MyViewHolder(View view) {
             super(view);
             universityName = view.findViewById(R.id.UniversityName);
             universityLocation = view.findViewById(R.id.UniversityLocation);
-            universityPopulation = view.findViewById(R.id.UniversityPopulation);
-            universityImage = view.findViewById(R.id.UniversityImage);
+            universitySize = view.findViewById(R.id.UniversitySize);
+            universityCost = view.findViewById(R.id.UniversityCost);
+            universityCategory = view.findViewById(R.id.UniversityCategory);
+            universityId = view.findViewById(R.id.UniversityId);
+            universityCompany = view.findViewById(R.id.UniversityCompany);
 
             view.setOnClickListener(this);
         }
+
 
         @Override
         public void onClick(View v) {
             int position = getBindingAdapterPosition();
             if (position != RecyclerView.NO_POSITION) {
-                Country university = universityList.get(position);
+                University university = universityList.get(position);
                 listener.onItemClick(university);
             }
         }
     }
+
+
+
 }
